1c411c26d47b5546afdfd2ea2407c7e9aa47ccae
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-06 16:56:44 -0500

    const title = 'timechain.academy';

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -4,7 +4,7 @@
 const lightCodeTheme = require('prism-react-renderer/themes/github');
 const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
-const title = 'podman desktop';
+const title = 'timechain.academy';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
