bbfb526c526eeb060b370365aca1a170f20242e3
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-06 20:30:04 -0500

    make: website: init config

---
+++ GNUmakefile
@@ -0,0 +1,10 @@
+.ONESHELL:
+.SILENT:
+-:## -
+	#NOTE: 2 hashes are detected as 1st column output with color
+	@awk 'BEGIN {FS = ":.*?## "} /^[a-zA-Z_-]+:.*?##/ {printf "\033[36m%-15s\033[0m %s\n", $$1, $$2}' $(MAKEFILE_LIST)
+
+
+-include website.mk
+# vim: set noexpandtab:
+# vim: set setfiletype make
---
+++ website.mk
@@ -0,0 +1,11 @@
+.PHONY:
+website:website-build website-serve## 	 website-build website-serve
+website-clear:## 	pushd website && yarn run clear
+	@pushd website && yarn run clear
+website-build:## 	website-build
+	@yarn run website:build
+website-serve:## 	website-serve
+	@killall -9 node 2>/dev/null || echo
+	@pushd website && npm run serve &
+# vim: set noexpandtab:
+# vim: set setfiletype make
