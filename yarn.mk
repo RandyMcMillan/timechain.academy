46bce26d4c08f8c5bc4b97d31d923820d1eba7a8
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-02-06 15:51:38 -0500

    yarn.mk: use yarn command explicitly

--- yarn.mk
+++ yarn.mk
@@ -1,16 +1,18 @@
+yarn-install:## 	yarn install
+	@yarn install
 yarn-run-build:## 	yarn run build
-	@$(PACKAGE_MANAGER) run build
+	@yarn run build
 yarn-run-compile:## 	yarn run compile
-	@$(PACKAGE_MANAGER) run compile
+	@yarn run compile
 yarn-run-test:## 	yarn run test
-	@$(PACKAGE_MANAGER) run test
+	@yarn run test
 yarn-run-typecheck:## 	yarn run typecheck
-	@$(PACKAGE_MANAGER) run typecheck
+	@yarn run typecheck
 yarn-run-watch:## 	yarn run watch
-	@$(PACKAGE_MANAGER) run watch
+	@yarn run watch
 yarn-run-website-build:
-	@$(PACKAGE_MANAGER) run website:build
+	@yarn run website:build
 yarn-run-website-dev:
-	@$(PACKAGE_MANAGER) run website:dev
+	@yarn run website:dev
 # vim: set noexpandtab:
 # vim: set setfiletype make
