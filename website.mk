afb30acd2e1fe9f07f2c5f2a8d4f8a9d2e6b5982
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-02-06 15:56:06 -0500

    website.mk: website-build: yarn-install ...

--- website.mk
+++ website.mk
@@ -2,7 +2,7 @@
 website:website-build website-serve## 	website-build website-serve
 website-clear:## 	pushd website && yarn run clear
 	@pushd website && yarn run clear
-website-build:## 	website-build
+website-build:yarn-install## 	yarn-install && yarn run website:build
 	@yarn run website:build
 website-serve:## 	website-serve
 	@killall -9 node 2>/dev/null || echo
